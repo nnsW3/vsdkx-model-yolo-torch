5bf92f4861e5ad3eb2af2eb9c4e5eb0fa746a28f
Administrator <admin@example.com>
2021-08-10 14:03:54 +0430

    LICENSE

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='vsdkx-model-yolo-torch',
-    url='https://gitlab.com/natix/cvison/vsdkx/vsdkx-model-yolo-torch',
+    url='https://github.com/natix-io/vsdkx-model-yolo-torch.git',
     author='Helmut',
     author_email='helmut@natix.io',
     namespace_packages=['vsdkx', 'vsdkx.model'],
